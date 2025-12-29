b25875f8e47d033c8281d16828bb8b1979a5a606
yee1819 <1244118445@qq.com>
2025-12-29 16:47:48 +0800

    首页更换为替补选择名单‘

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 export default function Home() {
   const router = useRouter()
     useEffect(() => {
-    router.replace("/predict/hakone/102")
+    router.replace("/predict/hakone/102/finalPredict")
   }, [])
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
