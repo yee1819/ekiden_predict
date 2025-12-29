05a045534811992bfd798156cd2f260aeae08984
yee1819 <1244118445@qq.com>
2025-12-29 17:57:09 +0800

    解决跳转问题

--- TODO.md
+++ TODO.md
@@ -8,14 +8,12 @@
 
 换下场不能上场
 
-
-
 ## 比赛结束后的预测率
-
 
 1. 十六人预测
    与
 2. 替补预测
 
-
 scp kirari@192.168.202.129:/home/kirari/Projects/ekiden_predict/.next.zip E:\
+
+scp kirari@192.168.202.129:/home/kirari/Projects/ekiden_predict/next-build-*.tar.gz  E:/
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { redirect } from 'next/navigation'
 export default function Home() {
   const router = useRouter()
-    useEffect(() => {
+  useEffect(() => {
     router.replace("/predict/hakone/102/finalPredict")
   }, [])
+  redirect('/predict/hakone/102/finalPredict')
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 
