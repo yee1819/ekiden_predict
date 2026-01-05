475887245a0340d4641a0c626c647ee737121278
yee1819 <1244118445@qq.com>
2026-01-05 13:54:29 +0800

    按钮顺序的更改

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,10 @@ export default function Home() {
   const zennihonTH = 57
 
   const links = [
+
+    { title: "箱根比赛结果", desc: "本届比赛结果", href: `/ekiden/hakone/${year}/result`, isDisabled: false },
+    { title: "箱根比赛排名", desc: "本届比赛各种数据排名", href: `/ekiden/hakone/${year}/rank`, isDisabled: false },
+    { title: "各个区间各种数据的排名", desc: "所有数据的展示", href: `/ekiden/hakone/${year}/interval`, isDisabled: false },
     { title: "区间分配", desc: "制定10个区间排兵布阵与时间预测", href: `/predict/hakone/${year}` },
     { title: "预测结果", desc: "浏览各位驿传粉丝的完整预测阵容", href: `/predict/hakone/${year}/result` },
     { title: "最终名单替换", desc: "根据首发名单进行替补更替", href: `/predict/hakone/${year}/finalPredict` },
@@ -22,9 +26,7 @@ export default function Home() {
     { title: "全日本驿传总成绩", desc: "查看全日本总成绩", href: `/ekiden/zennihon/${zennihonTH}/result` },
     { title: "全日本驿传成绩排行", desc: "查看全日本驿传各种数据的排行", href: `/ekiden/zennihon/${zennihonTH}/rank` },
 
-    { title: "箱根比赛结果", desc: "本届比赛结果", href: `/ekiden/hakone/${year}/result`, isDisabled: false },
-    { title: "箱根比赛排名", desc: "本届比赛各种数据排名", href: `/ekiden/hakone/${year}/rank`, isDisabled: false },
-    { title: "各个区间各种数据的排名", desc: "查看每个区间的排名", href: `/ekiden/hakone/${year}/interval`, isDisabled: false },
+
     { title: "箱根观察日志", desc: "网站开发箱根驿传的观察日志", href: `/post`, isDisabled: false }
   ]
   return (
